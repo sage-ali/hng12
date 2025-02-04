a6d2239b0375bb4d8b69f445f5f48b6ad9af4284
Ali Agboola <81039794+sage-ali@users.noreply.github.com>
2025-02-04 01:27:43 +0000

    chore(config-BE-01): Set up config for build

--- BE-stage-01/build-scripts/start-message.js
+++ BE-stage-01/build-scripts/start-message.js
@@ -1,27 +1,29 @@
-import chalk from 'chalk';
-import dotenv from 'dotenv';
+(async () => {
+  const chalk = await import('chalk');
+  const dotenv = require('dotenv');
 
-// Custom logger function
-const logger = (message, type = 'log') => {
-  if (type === 'error') {
-    console.error(chalk.red(message));
+  // Custom logger function
+  const logger = (message, type = 'log') => {
+    if (type === 'error') {
+      console.error(chalk.default.red(message));
+    } else {
+      console.log(chalk.default.green(message));
+    }
+  };
+
+  // Load environment variables from .env file
+  dotenv.config();
+
+  const NODE_ENV = process.env.NODE_ENV || 'development';
+
+  // Log the environment and server details
+  if (process.env.NODE_ENV === 'production') {
+    logger(chalk.default.green('Starting the production server...'));
   } else {
-    console.log(chalk.green(message));
+    logger(chalk.default.green('Starting the development server...'));
   }
-};
 
-// Load environment variables from .env file
-dotenv.config();
+  logger(chalk.default.green(`Environment: ${NODE_ENV}`));
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
-// Log the environment and server details
-if (process.env.NODE_ENV === 'production') {
-  logger(chalk.green('Starting the production server...'));
-} else {
-  logger(chalk.green('Starting the development server...'));
-}
-
-logger(chalk.green(`Environment: ${NODE_ENV}`));
-
-logger('Press Ctrl+C to stop the server.');
+  logger('Press Ctrl+C to stop the server.');
+})();
